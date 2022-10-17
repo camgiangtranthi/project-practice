ec28600158c0e4e51d39b598cdcfb4a65a73c1d1
camgiangtranthi <gtran527@gmail.com>
2022-10-17 17:12:58 +0700

    UI navbar

---
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className={"nav__header"}>
+                <div className={"nav__content"}>
+                    <Link to="/"><img src="/logo.webp" alt="logo" /></Link>
+                    <button className={"btn__create"}>Create new board</button>
+                </div>
+                <div className={"nav__profile"}>Avatar</div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
