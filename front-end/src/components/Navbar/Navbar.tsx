28d7317f2abacebef82bc26cb9d2219a20d6d688
camgiangtranthi <gtran527@gmail.com>
2022-10-18 13:28:15 +0700

    add popup profile into nav and fix css

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import PopupProfile from "../PopupProfile/PopupProfile";
 import "./Navbar.scss";
 
 const Navbar = () => {
+    const [isPopupProfile, setIsPopupProfile] = useState(false);
+
+    const handlePopupProfile = () => {
+        setIsPopupProfile(!isPopupProfile);
+    }
+
     return (
         <nav>
             <div className={"nav__header"}>
@@ -9,7 +17,10 @@ const Navbar = () => {
                     <Link to="/"><img src="/logo.webp" alt="logo" /></Link>
                     <button className={"btn__create"}>Create column</button>
                 </div>
-                <div className={"nav__profile"}>Avatar</div>
+                <div className={"nav__profile"}>
+                    <span className={"nav_profile-avatar"} onClick={handlePopupProfile }>Avatar</span>
+                    {isPopupProfile && <PopupProfile/>}
+                </div>
             </div>
         </nav>
     )
