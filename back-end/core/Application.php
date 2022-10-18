4343e774f49c351ce08f6679531da68614084702
bao8816 <nile1087@gmail.com>
2022-10-18 09:05:45 +0700

    restructure folder

---
+++ back-end/core/Application.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\core;
+
+class Application
+{
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public static Application $app;
+    public Database $db;
+    public static string $ROOT_DIR;
+
+    public function __construct($rootDir, array $config)
+    {
+        self::$app = $this;
+        self::$ROOT_DIR = $rootDir;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
+    }
+
+    public function run()
+    {
+        $this->router->resolve();
+    }
+}
---
+++ back-end/models/UserModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class UserModel extends Model
+{
+    public string $username = '';
+    public string $password = '';
+    public string $firstname = '';
+    public string $lastname = '';
+    public string $avatar = '';
+
+    public function rules(): array
+    {
+        return [
+            'username' => [self::RULE_REQUIRED, [self::RULE_UNIQUE, 'class' => self::class]],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
+            'firstname' => [],
+            'lastname' => [],
+            'avatar' => [],
+            'created_at' => [self::RULE_REQUIRED],
+        ];
+    }
+
+    public function tableName(): string
+    {
+        return 'users';
+    }
+
+    public function attributes(): array
+    {
+        return ['username', 'password', 'firstname', 'lastname', 'avatar', 'created_at'];
+    }
+}
