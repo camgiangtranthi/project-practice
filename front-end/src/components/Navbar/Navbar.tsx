ac938aaf33317af672def63d8ef0c33397fcabbc
camgiangtranthi <gtran527@gmail.com>
2022-10-18 16:10:51 +0700

    fix modules errors

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link } from "react-router-dom";
-import PopupProfile from "../PopupProfile/PopupProfile";
 import "./Navbar.scss";
 
 const Navbar = () => {
@@ -19,7 +18,6 @@ const Navbar = () => {
                 </div>
                 <div className={"nav__profile"}>
                     <span className={"nav_profile-avatar"} onClick={handlePopupProfile }>Avatar</span>
-                    {isPopupProfile && <PopupProfile/>}
                 </div>
             </div>
         </nav>
