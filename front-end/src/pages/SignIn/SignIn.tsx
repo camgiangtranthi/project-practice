8b46a81c210fa2f8dd09a584a5e99f9b3f414659
camgiangtranthi <gtran527@gmail.com>
2022-10-20 08:44:04 +0700

    call api

--- front-end/src/pages/SignIn/SignIn.tsx
+++ front-end/src/pages/SignIn/SignIn.tsx
@@ -1,70 +1,87 @@
-import { useForm, SubmitHandler,  } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
-import { AxiosError } from 'axios';
-import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import { useState, useEffect, useContext } from "react";
 import "./SignIn.scss";
-import { SignInErrorResponse } from '../../shared/models/auth';
+import { SignInErrorResponse, SignInData } from "../../shared/models/auth";
 import authApi from "../../api/authApi";
+import { UserContext } from "../../contexts/UserContext";
 
 type Inputs = {
   username: string;
   password: string;
 };
 
+class currentUser {}
+
 const SignIn = () => {
-  const [ errorMessage, setErrorMessage ] = useState<SignInErrorResponse>();
-  const [ signInSuccessfully, setSignInSuccessfully ] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<SignInErrorResponse>();
+  const [signInSuccessfully, setSignInSuccessfully] = useState<boolean>(false);
+  const userContext = useContext(UserContext);
   const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = async (data) => {
-        try {
-            const response = await authApi.signIn(data);
-            setSignInSuccessfully(true);
-            navigate('/');
-        } catch (error) {
-            const axiosError = error as AxiosError<SignInErrorResponse>;
-            if (axiosError.response) {
-                setErrorMessage(axiosError.response?.data);
-                setSignInSuccessfully(false);
-            }
-        }
+  useEffect(() => {
+    if (userContext?.userReponse) {
+      navigate("/");
     }
+  }, [userContext?.userReponse, navigate]);
 
-    useEffect(() => {
-        if (signInSuccessfully) {
-            setErrorMessage(undefined);
-        }
-    }, [signInSuccessfully]);
+  useEffect(() => {
+    if (signInSuccessfully) {
+      setErrorMessage(undefined);
+    }
+  }, [signInSuccessfully]);
+
+  const onSubmit: SubmitHandler<Inputs> = async (data: SignInData) => {
+    try {
+      const response = await authApi.signIn(data);
+      userContext?.setUserResponse(response.data);
+      setSignInSuccessfully(true);
+      navigate("/");
+    } catch (error) {
+      const axiosError = error as AxiosError<SignInErrorResponse>;
+      if (axiosError.response) {
+        setErrorMessage(axiosError.response?.data);
+        setSignInSuccessfully(false);
+      }
+    }
+  };
 
   return (
-      <div className="signin__wrapper">
-        <div className="signin">
-          <div className="signin__header">
-            <h2 className="signin__title">Sign In</h2>
-          </div>
-          <form className="signin__form" onSubmit={handleSubmit(onSubmit)}>
-              <label htmlFor="username">Username </label>
-              <input id="username" {...register("username", { required: true})} />
-              {errors.username && <span className="error-message">Username is required</span>}
-              {errorMessage?.errors.email && <p className="error-message">{errorMessage?.errors.email}</p>}
-              <label htmlFor="password">Password </label>
-              <input id="password" {...register("password", { required: true })} />
-              {errors.password && <span>Password is required</span>}
-              {errorMessage?.errors.password && <p className="error-message">{errorMessage?.errors.password}</p>}
-              <button className="signin__btn">Sign in</button>
-              <div className="signin__footer">
-                  <span>Don't have an account?</span>
-                  <Link to="/signup">Sign up</Link>
-              </div>
-          </form>
+    <div className="signin__wrapper">
+      <div className="signin">
+        <div className="signin__header">
+          <h2 className="signin__title">Sign In</h2>
         </div>
+        <form className="signin__form" onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="username">Username </label>
+          <input id="username" {...register("username", { required: true })} />
+          {errors.username && (
+            <span className="error-message">Username is required</span>
+          )}
+          {errorMessage?.errors.email && (
+            <p className="error-message">{errorMessage?.errors.email}</p>
+          )}
+          <label htmlFor="password">Password </label>
+          <input id="password" {...register("password", { required: true })} />
+          {errors.password && <span>Password is required</span>}
+          {errorMessage?.errors.password && (
+            <p className="error-message">{errorMessage?.errors.password}</p>
+          )}
+          <button className="signin__btn">Sign in</button>
+          <div className="signin__footer">
+            <span>Don't have an account?</span>
+            <Link to="/signup">Sign up</Link>
+          </div>
+        </form>
       </div>
+    </div>
   );
 };
 
--- front-end/src/shared/models/user.ts
+++ front-end/src/shared/models/user.ts
@@ -1,22 +1,23 @@
 export interface UserRequest {
-    email: string,
-    password: string
+  email: string;
+  password: string;
 }
 export interface User {
-    id: string,
-    username: string,
-    firstname?: string,
-    lastname?: string,
-    avatar?: string,
-    email: string
+  id: string;
+  username: string;
+  firstname?: string;
+  lastname?: string;
+  avatar?: string;
+  email: string;
+  password: string;
 }
 
-export interface UserResponse{
-    data: string;
-    user: User,
-    token: string
+export interface UserResponse {
+  data: any;
+  user: User;
+  token: string;
 }
 
 export interface UserLocal {
-    user: User,
+  user: User;
 }
