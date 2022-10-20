c5df4117516571c11bdb3c2e09362cae1ec83688
camgiangtranthi <gtran527@gmail.com>
2022-10-20 15:42:39 +0700

    call api sign out function

--- front-end/src/components/PopupProfle/PopupProfile.tsx
+++ front-end/src/components/PopupProfle/PopupProfile.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import { CloseOutlined } from "@ant-design/icons";
+import { useContext, useState } from "react";
 import "./PopupProfile.scss";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
 
 const PopupProfile = () => {
-  const [isPopupProfile, setIsPopupProfile] = useState(true);
+  const currentUserContext = useContext(UserContext);
 
-  const handlePopupProfile = () => {
-    setIsPopupProfile(false);
+  const handleLogout = () => {
+    localStorage.removeItem("current_user");
+    currentUserContext?.setUserResponse(
+      JSON.parse(localStorage.getItem("current_user") || "false")
+    );
   };
 
   return (
@@ -20,7 +23,7 @@ const PopupProfile = () => {
           <div className={"profile__avatar"}>Avatar</div>
           <div className={"profile__username"}>Le Thai Phuc Bao</div>
         </div>
-        <div className={"profile__button"}>
+        <div className={"profile__button"} onClick={handleLogout}>
           <Link to={"/signin"}>Log out</Link>
         </div>
       </div>
