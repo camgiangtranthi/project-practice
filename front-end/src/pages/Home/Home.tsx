b8038365f9cff995fee714a5863427bc88d7b4a9
camgiangtranthi <gtran527@gmail.com>
2022-10-21 07:44:34 +0700

    prevent back to sign in

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("current_user")) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="home">
       <Navbar
