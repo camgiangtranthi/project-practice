05a5be6967eddc0d59dc8f82af2e680d795dc222
camgiangtranthi <gtran527@gmail.com>
2022-10-25 10:12:10 +0700

    fix call api update column (doing)

--- front-end/src/components/Column/Column.tsx
+++ front-end/src/components/Column/Column.tsx
@@ -1,79 +1,70 @@
 import {DeleteOutlined, PlusOutlined} from "@ant-design/icons";
-import {ChangeEvent, useState, useCallback} from "react";
+import { useState} from "react";
 import "./Column.scss";
-import {column, columnCreateRequest, columnUpdateRequest} from "../../shared/models/column";
-import {api} from "../../api/column";
+import {columnCreateRequest} from "../../shared/models/column";
 
 import AutoSave from "../AutoSave/AutoSave";
 
 
 interface IColumnProps {
 	columns: columnCreateRequest[];
-	setColumns: (columns: columnCreateRequest[]) => void;
 	addColumn: (column: columnCreateRequest) => void;
+	handleDeleteColumn: (id: any) => void;
+	handleUpdateColumn: (column: ColumnInterface) => void;
 }
 
 export const LOCAL_STORAGE_KEY = "columns";
 
-export type Column = { title: string };
+export type ColumnInterface = { title: string };
 
-const Column = (props: IColumnProps, Column : Column) => {
-	const [columnTitle, setColumnTitle] = useState<Column>(
+const Column = (props: IColumnProps) => {
+	const [columnTitle, setColumnTitle] = useState<ColumnInterface>(
 		() => ({title : window.localStorage.getItem(LOCAL_STORAGE_KEY) || ""})
 	);
 	
-	const handleUpdateColumn = async (column: columnUpdateRequest) => {
-		const response = await api.put(`/columns/${column.id}`, column);
-		const {id, title} = response.data;
-		props.setColumns(
-			props.columns.map((column) => {
-				return column.id === id ? {...response.data} : column;
-			})
-		);
-	}
-	
-	const handleDeleteColumn = async (id: any) => {
-		await api.delete(`/columns/${id}`);
-		const newColumns = props.columns.filter((column) => column.id !== id);
-		// props.setColumns(newColumns);
-	}
-	
-	const onColumnTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-		console.log(event.target.value);
-		event.preventDefault();
-		handleUpdateColumn({id: event.target.id, title: event.target.value});
-		// @ts-ignore
-		setColumnTitle("");
-	}
-	
 	const onDeleteColumn = (id: any) => {
-		handleDeleteColumn(id);
+		props.handleDeleteColumn(id);
+		console.log(id);
 	}
+	
+	const onUpdateColumn = (e: any) => {
+		
+		e.preventDefault();
+		if (columnTitle.title === "") return;
+		props.handleUpdateColumn(columnTitle);
+		setColumnTitle({title: ""});
+	};
 	
 	return (
 		<div className={"column"}>
 			{
 				props.columns.map((column, id) => {
+					// @ts-ignore
 					return (
 						<div className={"column__container"} key={id}>
 							<div className={"column__header"}>
-								<form>
+								<form onSubmit={() => onUpdateColumn(column.id)}>
 									<input
 										type="text"
 										className={"column__title"}
 										placeholder={"Enter column title"}
-										onChange={onColumnTitleChange}
 										value={columnTitle.title}
+										onChange={(e) => setColumnTitle({title: e.target.value})}
 									/>
 									<AutoSave column={column}/>
+									<button className={"column__header-icon"}>
+										save
+									</button>
 								</form>
-								<div className={"column__header-icon"} onClick={() => onDeleteColumn(id)}>
-									<DeleteOutlined/>
-								</div>
 							</div>
 							<div className={"column__addnew"}>
-								<PlusOutlined/>
-								<button className={"btn__add-task"}>Add a card</button>
+								<div>
+									<PlusOutlined/>
+									<button className={"btn__add-task"}>Add a card</button>
+								</div>
+								<div className={"column__delete-icon"} onClick={() => onDeleteColumn(column.id)}>
+									<DeleteOutlined/>
+								</div>
 							</div>
 						</div>
 					)
--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,90 +1,101 @@
-import {useState, useEffect, ChangeEvent} from "react";
-import { Link } from "react-router-dom";
-import { v4 as uuid } from 'uuid';
+import {useState, useEffect } from "react";
+import {Link} from "react-router-dom";
+import {v4 as uuid} from 'uuid';
 import "./Navbar.scss";
 import PopupProfile from "../PopupProfle/PopupProfile";
 import Column from "../Column/Column";
-import { api } from "../../api/column";
+import {api} from "../../api/column";
 
 
 const Navbar = () => {
-  const [isPopupProfile, setIsPopupProfile] = useState(false);
-  const [title, setTitle] = useState("");
-  // @ts-ignore
-  const [column, setColumn] = useState<Column>({title: ""});
-  const [columns, setColumns] = useState([]);
-  
-  const retrieveColumns = async () => {
-    const response = await api.get("/columns");
-    return response.data;
-  };
-
-  const handlePopupProfile = () => {
-    setIsPopupProfile(!isPopupProfile);
-  };
-  
-  // @ts-ignore
-  const addColumnHandler = async (column) => {
-    console.log(column);
-    const request = {
-      id: uuid(),
-      ...column,
-    };
-    
-    const response = await api.post("/columns", request);
-    const {id, title} = response.data;
-    // @ts-ignore
-    setColumns([...columns, response.data]);
-    setColumn({id, title});
-  };
-  
-  useEffect(() => {
-    const getAllColumns = async () => {
-        const allColumns = await retrieveColumns();
-        if (allColumns) setColumns(allColumns);
-    }
-    
-    getAllColumns();
-  }, []);
-  
-  // @ts-ignore
-  const addColumn = () => {
-    addColumnHandler(column);
-  }
-
-  return (
-    <nav>
-      <div className={"nav__header"}>
-        <div className={"nav__content"}>
-          <Link to="/">
-            <img src="/logo.webp" alt="logo" />
-          </Link>
-          <button className={"btn__create"} onClick={addColumn}>Create column</button>
-        </div>
-        <div className={"nav__profile"}>
+	const [isPopupProfile, setIsPopupProfile] = useState(false);
+	// @ts-ignore
+	const [column, setColumn] = useState<Column>({title: ""});
+	const [columns, setColumns] = useState([]);
+	
+	const retrieveColumns = async () => {
+		const response = await api.get("/columns");
+		return response.data;
+	};
+	
+	const handlePopupProfile = () => {
+		setIsPopupProfile(!isPopupProfile);
+	};
+	
+	// @ts-ignore
+	const addColumnHandler = async (column) => {
+		const request = {
+			id: uuid(),
+			...column,
+		};
+		
+		const response = await api.post("/columns", request);
+		const {id, title} = response.data;
+		// @ts-ignore
+		setColumns([...columns, response.data]);
+		setColumn({id, title});
+	};
+	
+	const handleDeleteColumn = async (id: any) => {
+		await api.delete(`/columns/${id}`);
+		// @ts-ignore
+		const newColumns = columns.filter((column) => column.id !== id);
+		// @ts-ignore
+		setColumns(newColumns);
+	}
+	
+	const handleUpdateColumn = async (column: any) => {
+		const response = await api.put(`/columns/${column.id}`, column);
+		const { id } = response.data;
+		// @ts-ignore
+		setColumns(columns.map((column) => {
+			// @ts-ignore
+			return column.id === id ? { ...response.data } : column;
+		}));
+	}
+	
+	useEffect(() => {
+		const getAllColumns = async () => {
+			const allColumns = await retrieveColumns();
+			if (allColumns) setColumns(allColumns);
+		}
+		
+		getAllColumns();
+	}, []);
+	
+	// @ts-ignore
+	const addColumn = () => {
+		addColumnHandler(column);
+	}
+	
+	return (
+		<nav>
+			<div className={"nav__header"}>
+				<div className={"nav__content"}>
+					<Link to="/">
+						<img src="/logo.webp" alt="logo"/>
+					</Link>
+					<button className={"btn__create"} onClick={addColumn}>Create column</button>
+				</div>
+				<div className={"nav__profile"}>
           <span className={"nav_profile-avatar"} onClick={handlePopupProfile}>
             <img
-              className={"profile__avatar"}
+	            className={"profile__avatar"}
             />
           </span>
-          {isPopupProfile && (
-            <PopupProfile
-              // userResponse={JSON.parse(
-              //   localStorage.getItem("current_user") || ""
-              // )}
-            />
-          )}
-        </div>
-      </div>
-      <Column
-          columns={columns}
-          column={column}
-          // @ts-ignore
-          setColumns={setColumns}
-          addColumn={addColumn}
-      />
-    </nav>
-  );
+					{isPopupProfile && (
+						<PopupProfile/>
+					)}
+				</div>
+			</div>
+			<Column
+				columns={columns}
+				addColumn={addColumn}
+				handleDeleteColumn={handleDeleteColumn}
+				handleUpdateColumn={handleUpdateColumn}
+			/>
+		</nav>
+	);
 };
 
 export default Navbar;
